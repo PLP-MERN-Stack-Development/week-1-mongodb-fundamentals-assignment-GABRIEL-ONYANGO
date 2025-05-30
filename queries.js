a4c13337a500346ed3260029909c722de91d8081
GABRIEL-ONYANGO <143998221+GABRIEL-ONYANGO@users.noreply.github.com>
2025-05-30 09:59:25 +0300

    Update queries.js

--- queries.js
+++ queries.js
@@ -63,3 +63,14 @@ db.books.aggregate([
   }
 ]);
 
+Indexing
+// Create index on title
+db.books.createIndex({ title: 1 });
+
+// Create compound index on author and published_year
+db.books.createIndex({ author: 1, published_year: -1 });
+
+// Use explain() to analyze performance
+db.books.find({ title: '1984' }).explain("executionStats");
+
+
